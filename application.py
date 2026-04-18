374a1600bb9fc9e00524d1eb150cc747ac954e5b
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2026-04-18 16:35:09 -0500

    Test deployment on AWS CodePipeline x3

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import funtions as f
 from flask import Flask, jsonify
 
-# Heroes API -- By: @dso-uniandes
+# Heroes API -- By: @dso-uniandes (Daniel)
 
 application = Flask(__name__)
 data = f.load_file('./heroes.csv')
